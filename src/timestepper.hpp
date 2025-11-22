dbf545d1f515d2ed094b587a695112c1ec139ca3
Leon_ <e12125027@student.tuwien.ac.at>
2025-11-22 12:36:12 +0100

    ImprovedEuler

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -20,6 +20,30 @@ namespace ASC_ode
     virtual void DoStep(double tau, VectorView<double> y) = 0;
   };
 
+  class ImprovedEuler : public TimeStepper
+  {
+    Vector<> m_vecf;
+    Vector<> m_y_hat;  // adding variable storage
+  public:
+    ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs)
+    : TimeStepper(rhs), m_vecf(rhs->dimF()), m_y_hat(rhs->dimX()) {}
+    void DoStep(double tau, VectorView<double> y) override
+    {
+      //evaluating f at current y
+      this->m_rhs->evaluate(y, m_vecf);
+
+      // intermediate point
+      m_y_hat = y;
+      m_y_hat += tau/2 * m_vecf;
+
+      // evaluating at y_hat
+      this->m_rhs->evaluate(m_y_hat, m_vecf);
+
+      // actual update: y += tau * f(y_hat)
+      y += tau * m_vecf;
+    }
+  };
+
   class ExplicitEuler : public TimeStepper
   {
     Vector<> m_vecf;
