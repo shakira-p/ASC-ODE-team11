3382f63d8892cd1b6bd019778e838d1915a6f99c
Shakira Fibi Pannier <e12509817@student.tuwien.ac.at>
2025-12-07 12:38:19 +0100

    fix: remove comment

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -79,36 +79,6 @@ namespace ASC_ode
     }
   };
 
-/**
-class CrankNicolson : public TimeStepper
-  {
-    std::shared_ptr<NonlinearFunction> m_equ;
-    std::shared_ptr<Parameter> m_tau;
-    std::shared_ptr<ConstantFunction> m_yold;
-  public:
-    CrankNicolson(std::shared_ptr<NonlinearFunction> rhs)
-    : TimeStepper(rhs), m_tau(std::make_shared<Parameter>(0.0))
-    {
-      m_yold = std::make_shared<ConstantFunction>(rhs->dimX());
-      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
-    }
-
-    void DoStep(double tau, VectorView<double> y) override
-    {
-      m_yold->set(y);
-      m_tau->set(tau);
-	  this->m_rhs->evaluate(y, m_rhs);
-
-	  this->m_rhs->evaluate(m_rhs, m_rhs_new);
-
-	  y += tau/2 * (m_rhs + m_rhs_new);
-	  auto tau_param = std::make_shared<Parameter>(0.5 * tau);
-	  m_equ = ynew - m_yold - tau_param * (m_rhs + m_rhs_new);
-
-      NewtonSolver(m_equ, y);
-    }
-  };
-*/
 
 class CrankNicolson : public TimeStepper
 {
