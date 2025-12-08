3b93d7b44d1308f18d7cc858b68dbf09efce6caa
e12507337 <e12507337@student.tuwien.ac.at>
2025-12-08 01:40:31 +0100

    electric network report

--- demos/plotcircuit.py
+++ demos/plotcircuit.py
@@ -1,22 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-type = ["explicit", "implicit", "improved", "crank"]
-step = "100"
-data = []
+types = ["explicit", "implicit", "improved", "crank"]
+type = types[3]
+# steps = ["100", "1000", "10000"]
+steps = ["1000", "5000", "10000"]
+# steps = ["5000", "10000", "15000"]
+# steps = ["100", "1000", "10000", "20000"]
+step = steps[0]
+set = "2"
+data1, data2, data3, data4 = [], [], [], []
+data = [data1, data2, data3, data4]
 
-colors = ['#1f77b4', '#ff7f0e', '#d62728', '#2ca02c']
+colors = ['#1f77b4', '#ff7f0e', '#d62728', '#2ca02c', '#9467bd']
+
+for i in range(len(steps)):
+    data[i].append(np.loadtxt(f"demos/data/electric_circuit/set{set}/circuit_{type}{steps[i]}.txt", usecols=(0, 1, 2)))
+
+for i in range(len(data1)):
+    plt.plot(data3[i][:,0], np.cos(100*np.pi*data3[i][:,0]), label=f'U_0', color=colors[0])
+    plt.plot(data1[i][:,0], data1[i][:,1], label=f'U_C steps = {steps[0]}', color=colors[1])
+    plt.plot(data2[i][:,0], data2[i][:,1], label=f'U_C steps = {steps[1]}', color=colors[2])
+    plt.plot(data3[i][:,0], data3[i][:,1], '--',label=f'U_C steps = {steps[2]}', color=colors[3])
+    # plt.plot(data4[i][:,0], data4[i][:,1], label=f'U_C steps = {steps[2]}', color=colors[4])
 
 
-data.append(np.loadtxt(f"data/circuit_explicit.txt", usecols=(0, 1, 2)))
-
-for i in range(len(data)):
-    plt.plot(data[i][:,0], data[i][:,1], label=f'U_0')
-    plt.plot(data[i][:,0], data[i][:,2], label=f'U_C')
-plt.xlabel('time')
-plt.ylabel('Velocity')
-plt.title('Voltage change in time')
-plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+plt.xlabel('Time [s]')
+plt.ylabel('Voltage [V]')
+plt.xlim(0, 0.04)
+# plt.ylim(-1.5, 1.75)
+plt.title(f'Electric Circuit - Crank-Nicolson Method')
+plt.legend(loc='upper right')
 plt.grid()
-plt.savefig(f"./plots/ElectricCircuit_{step}.png")
+plt.savefig(f"demos/plots/ElectricCircuit_set2_crank2.png")
 plt.show()
